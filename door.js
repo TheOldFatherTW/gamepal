ac32dfde5f2db05d4ae34c76e2d85776ef9188b3
TheOldFatherTW <weslie0705@gmail.com>
2026-09-08 11:59:08 +0800

    Keep the watch playhead on the time the user scrubbed to.

--- door.js
+++ door.js
@@ -1002,7 +1002,7 @@
     q.set("video", item.id);
     q.set("game", openGame || "");
     q.set("k", key);
-    q.set("wv", "1");
+    q.set("wv", "2");
     if (item.start) q.set("t", String(item.start));
     return "./watch.html?" + q.toString() + "#k=" + encodeURIComponent(key);
   }
